bdec59329184657192c044a7544de581a7456fd9
Agustin notebook <ronedev25@gmail.com>
2022-08-01 18:33:52 -0300

    feat: getting the vacancies from the database and sending them to the home view

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,16 @@
-exports.showHome = (req, res)=>{
+const mongoose = require('mongoose')
+const Vacant = mongoose.model('Vacant')
+
+exports.showHome = async (req, res, next)=>{
+
+    const vacantes = await Vacant.find().lean() //.lean para obtener un json
+
+    if(!vacantes) return next()
     res.render('home', {
         page: 'devJobs',
         tagline: 'Encuentra y pública trabajos para desarrolladores web',
         bar: true,
-        button: true
+        button: true,
+        vacantes
     })
 }
